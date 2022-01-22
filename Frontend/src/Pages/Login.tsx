5c3276ad8969eda09ff885c0371a8e75a601418a
Asel <peirisasel373@Gmail.com>
2022-01-22 12:49:02 +0530

    Adding account type dropdown

--- Frontend/src/Pages/Login.tsx
+++ Frontend/src/Pages/Login.tsx
@@ -18,14 +18,13 @@ export default function Login() {
                             <div className="mb-6">
                                 <div className="flex justify-between mb-2">
                                     <label htmlFor="password" className="text-sm text-gray-600 dark:text-gray-400">Password</label>
-                                    <a href="#!" className="text-sm text-gray-400 focus:outline-none focus:text-indigo-500 hover:text-indigo-500 dark:hover:text-indigo-300">Forgot password?</a>
                                 </div>
                                 <input type="password" name="password" id="password" placeholder="Your Password" className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" />
                             </div>
                             <div className="mb-6">
                                 <button type="button" className="w-full px-3 py-4 text-white bg-sky-300 rounded-md focus:bg-sky-500 hover:bg-sky-500 focus:outline-none">Sign in</button>
                             </div>
-                            <p className="text-sm text-center text-gray-400">Don&#x27;t have an account yet? <a href="/register" className="text-indigo-400 focus:outline-none focus:underline focus:text-indigo-500 dark:focus:border-indigo-800">Sign up</a>.</p>
+                            <p className="text-sm text-center text-gray-400">Don&#x27;t have an account yet? <a href="/register" className="text-sky-400 focus:outline-none focus:underline focus:text-sky-500 dark:focus:border-sky-800">Sign up</a>.</p>
                         </form>
                     </div>
                 </div>
--- Frontend/src/Pages/Register.tsx
+++ Frontend/src/Pages/Register.tsx
@@ -18,6 +18,13 @@ export default function Register() {
                                 <label htmlFor="email" className="block mb-2 text-sm text-gray-600 dark:text-gray-400">Email Address</label>
                                 <input type="email" name="email" id="email" placeholder="you@company.com" className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" />
                             </div>
+                            <div className="mb-6" >
+                                <label htmlFor="type" className="block mb-2 text-sm text-gray-600 dark:text-gray-400">Account Type</label>
+                                <select name="type" id="type" className='w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500'>
+                                    <option value="volvo">Actor</option>
+                                    <option value="saab">Recruiter</option>
+                                </select>
+                            </div>
                             <div className="mb-6">
                                 <div className="flex justify-between mb-2">
                                     <label htmlFor="password" className="text-sm text-gray-600 dark:text-gray-400">Password</label>
@@ -27,7 +34,7 @@ export default function Register() {
                             <div className="mb-6">
                                 <button type="button" className="w-full px-3 py-4 text-white bg-sky-300 rounded-md focus:bg-sky-500 hover:bg-sky-500 focus:outline-none">Register</button>
                             </div>
-                            <p className="text-sm text-center text-gray-400">Already have an account? <a href="/login" className="text-indigo-400 focus:outline-none focus:underline focus:text-indigo-500 dark:focus:border-indigo-800">Login</a>.</p>
+                            <p className="text-sm text-center text-gray-400">Already have an account? <a href="/login" className="text-sky-400 focus:outline-none focus:underline focus:text-sky-500 dark:focus:border-sky-800">Login</a>.</p>
                         </form>
                     </div>
                 </div>
